73de3a86d8a514656d55e1b3b090f0ec1f462862
akashamba <akashambashankar@gmail.com>
2026-02-19 12:26:58 -0500

    working prototype: fetch emails from gmail api, add them to db, fetch from db and display on client

--- src/app/_components/post.tsx
+++ src/app/_components/post.tsx
@@ -1,50 +1,32 @@
 "use client";
 
 import { useState } from "react";
-
+import type { Article } from "~/server/db/schema";
 import { api } from "~/trpc/react";
 
-export function LatestPost() {
-  const [latestPost] = api.post.getLatest.useSuspenseQuery();
+export default function Post() {
+  const [article, setArticle] = useState<Article>();
 
-  const utils = api.useUtils();
-  const [name, setName] = useState("");
-  const createPost = api.post.create.useMutation({
-    onSuccess: async () => {
-      await utils.post.invalidate();
-      setName("");
-    },
-  });
+  const syncOne = api.articles.syncOneArticle.useMutation();
+
+  const syncAndFetchOneArticle = async () => {
+    const newArticle: Article = (await syncOne.mutateAsync()) as Article;
+    if (newArticle) {
+      setArticle(newArticle);
+    }
+  };
 
   return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
+    <div>
+      {article ? (
+        <div dangerouslySetInnerHTML={{ __html: article.content }}></div>
       ) : (
-        <p>You have no posts yet.</p>
+        <div>No article yet</div>
       )}
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          createPost.mutate({ name });
-        }}
-        className="flex flex-col gap-2"
-      >
-        <input
-          type="text"
-          placeholder="Title"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="w-full rounded-full bg-white/10 px-4 py-2 text-white"
-        />
-        <button
-          type="submit"
-          className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-          disabled={createPost.isPending}
-        >
-          {createPost.isPending ? "Submitting..." : "Submit"}
-        </button>
-      </form>
+
+      <button onClick={syncAndFetchOneArticle} disabled={syncOne.isPending}>
+        {syncOne.isPending ? "Syncing..." : "Hello"}
+      </button>
     </div>
   );
 }
